b3ce835ae41050b125eec6552676159dc0af7333
WaTharPhat <watharphat@gmail.com>
2023-03-29 21:40:53 +0700

    loaded commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className="layout">
+            <img src="assets/prof.jpg" alt="profile/"></img>
+            <h1 className="name">Paul</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
