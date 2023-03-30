10506dbdce03474e132482bb076006ce2e84fd51
WaTharPhat <watharphat@gmail.com>
2023-03-30 12:11:27 +0700

    loaded commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import Home from "./components/Home";
 import Todo from "./components/Todo";
+
 import TodoPage from "./pages/TodoPage"
+
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import TodayPage from "./pages/TodayPage"
+import CalendarPage from "./pages/CalendarPage"
+import HomePage from "./pages/HomePage"
+
 function App() {
   return (
-    <TodoPage/>
+    // <BrowserRouter>
+    <Routes>
+      <Route exect path="/" element={<HomePage />} />
+      <Route path="/todo" element={<TodoPage/>} />
+      <Route path="/today" element={<TodayPage/>} />
+      <Route path="/calendar" element={<CalendarPage/>} />
+
+    </Routes>
+    // </BrowserRouter>
   );
 }
 
